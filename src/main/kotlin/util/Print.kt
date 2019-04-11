15f393209c46975dd16b4fb7d4076b1d8e3cc6ab
ednaldomartins <martinsednaldo@outlook.com>
2019-04-11 01:30:31 -0300

    método para print de matriz.

--- src/main/kotlin/util/Print.kt
+++ src/main/kotlin/util/Print.kt
@@ -10,5 +10,15 @@ class Print
             for ( i in 0 .. (intArray.size-1) )
                 print(" " + intArray[i])
         }
+
+        fun printMatriz(intMatriz: Array<IntArray>)
+        {
+            for (i in 0 .. intMatriz.size-1)
+            {
+                print("\n")
+                for (j in 0..intMatriz.size - 1)
+                    print(" ${intMatriz[i][j]}")
+            }
+        }
     }
 }
