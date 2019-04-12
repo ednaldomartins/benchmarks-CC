d56d253be73e26cabda148aabbe3422b6ed2ec16
ednaldomartins <martinsednaldo@outlook.com>
2019-04-12 16:57:38 -0300

    Método para printar array de float.

--- src/main/kotlin/util/Print.kt
+++ src/main/kotlin/util/Print.kt
@@ -20,5 +20,15 @@ class Print
                     print(" ${intMatriz[i][j]}")
             }
         }
+
+        fun printMatriz(floatMatriz: Array<FloatArray>)
+        {
+            for (i in 0 .. floatMatriz.size-1)
+            {
+                print("\n")
+                for (j in 0..floatMatriz.size - 1)
+                    print(" ${floatMatriz[i][j]}")
+            }
+        }
     }
 }
