a0f05ac92ccd46f047043bfa28395139f039e4fe
ednaldomartins <martinsednaldo@outlook.com>
2019-04-12 16:58:59 -0300

    menu inicial básico para escolha do benchmark.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,42 @@
+import control.BenchmarksController
 import model.Fannkuch
+import model.Spectralnorm
+import java.util.*
 
 fun main()
 {
-    print("teste")
-    var bm = Fannkuch()
+    //var bm:Benchmark
 
     //teste do site online: https://benchmarksgame-team.pages.debian.net/benchmarksgame/description/fannkuchredux.html
-    var v1 = intArrayOf(3,1,0,4,2)
-    var v2 = intArrayOf(2,5,1,3,0,4)
+    //var v1 = intArrayOf(3,1,0,4,2)
+    //bm = Fannkuch()
+    //bm.criarTeste(5)
+    //bm.vetor = v1
+    //bm.benchmark()
 
-    bm.benchmark(v1)
+
+    //teste 2: spectralnorm
+    //bm = Spectralnorm()
+    //bm.criarTeste(10)
+    var bc = BenchmarksController()
+    print("Escolha um dos teste abaixo:" +
+            "\n1. Fannkuch" +
+            "\n2. Spectralnorm" +
+            "\n")
+    val input =Integer.parseInt( readLine()!! )
+    when(input )
+    {
+        1 -> {
+            println("Digite o tamanho do teste")
+            val tamanho = Integer.parseInt( readLine()!! )
+            bc.benchmarks(Fannkuch(), tamanho)
+        }
+        2 ->{
+            println("Digite o tamanho do teste")
+            val tamanho = Integer.parseInt( readLine()!! )
+            bc.benchmarks(Spectralnorm(), tamanho)
+        }
+        else-> println("Digite um valor existente no Menu")
+    }
+
 }
