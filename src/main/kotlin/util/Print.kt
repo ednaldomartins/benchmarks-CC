3058d6a01f8a39704cf8b66b4a9c1b09a9c0dc52
ednaldomartins <martinsednaldo@outlook.com>
2019-04-13 13:19:13 -0300

    print para Array do tipo Double.

--- src/main/kotlin/util/Print.kt
+++ src/main/kotlin/util/Print.kt
@@ -6,28 +6,41 @@ class Print
     {
         fun printArray(intArray: IntArray)
         {
-            print("\n\nIntArray: ")
+            print("\n\nIntArray:")
             for ( i in 0 .. (intArray.size-1) )
                 print(" " + intArray[i])
         }
 
         fun printMatriz(intMatriz: Array<IntArray>)
         {
-            for (i in 0 .. intMatriz.size-1)
+            print("\n\nIntMatriz:")
+            for (i in 0 .. (intMatriz.size-1))
             {
                 print("\n")
-                for (j in 0..intMatriz.size - 1)
+                for (j in 0 .. (intMatriz.size - 1))
                     print(" ${intMatriz[i][j]}")
             }
         }
 
         fun printMatriz(floatMatriz: Array<FloatArray>)
         {
-            for (i in 0 .. floatMatriz.size-1)
+            print("\n\nFloatMatriz:")
+            for (i in 0 .. (floatMatriz.size-1))
             {
                 print("\n")
-                for (j in 0..floatMatriz.size - 1)
+                for (j in 0 .. (floatMatriz.size - 1))
                     print(" ${floatMatriz[i][j]}")
+            }
+        }
+
+        fun printMatriz(doubleMatriz: Array<DoubleArray>)
+        {
+            print("\n\nFloatMatriz:")
+            for (i in 0 .. (doubleMatriz.size-1))
+            {
+                print("\n")
+                for (j in 0 .. (doubleMatriz.size - 1))
+                    print(" ${doubleMatriz[i][j]}")
             }
         }
     }
