ecb0305b77276a58fad5d490ce62a62c2bd2a70b
ednaldomartins <martinsednaldo@outlook.com>
2019-04-22 00:24:09 -0300

    Calcular o tempo.

--- src/main/kotlin/control/BenchmarksController.kt
+++ src/main/kotlin/control/BenchmarksController.kt
@@ -4,9 +4,15 @@ import model.Benchmark
 
 class BenchmarksController
 {
-    fun benchmarks(benchmark: Benchmark, tamanho:Int)
+    fun benchmarks(benchmark: Benchmark, tamanho: Int)
     {
         benchmark.criarTeste(tamanho)
+
+        val tempoInicial = System.currentTimeMillis()
         benchmark.benchmark()
+        val tempoFinal = System.currentTimeMillis()
+
+        val tempo = (tempoFinal - tempoInicial)
+        println( "\n\n\n\n\ntempo: $tempo milesegundos" )
     }
 }
